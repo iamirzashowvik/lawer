c1559b7d80e5247129892096a3e4dc0aea2bd71b
iamirzashowvik <showvikmirza@gmail.com>
2021-06-27 03:15:40 +0600

    last

---
+++ lib/laws/lawlists.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Lawlists extends StatefulWidget {
+  @override
+  _LawlistsState createState() => _LawlistsState();
+}
+
+class _LawlistsState extends State<Lawlists> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
